2567bada00f1c93c3c439c5cf7217c68feecdae1
MatheusKyoshiShoji <142830615+MatheusKyoshiShoji@users.noreply.github.com>
2025-04-22 15:59:30 -0300

    #1 dashboard | fetchData

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
+import { cookies } from "next/headers";
 import { Card } from "../components/card/card";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('sf_access_token');
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/sf/metadata`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ sessionId: accessToken?.value}),
+    })
+
     return (
     <div className="w-11/12 m-auto">
         <div className="p-2 bg-gray-50 shadow-2xl rounded-3xl mt-3">
---
+++ src/entities/metadata.ts
@@ -0,0 +1,6 @@
+export interface Metadata {
+    createdByName: string,
+    createdDate: Date,
+    fileName: string,
+    type: string
+}
